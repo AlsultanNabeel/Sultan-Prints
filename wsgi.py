02106d5fca5fc2b009dd11c7259c13ad615bfafa
Nabeel Alsultan <ohnabeel@gmail.com>
2025-06-13 19:57:48 +0300

    Update wsgi.py
    ترجيع للاصل

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,6 @@
 # wsgi.py
 import os
 from app import create_app
-from flask_migrate import upgrade
-from app.extensions import db
 
 # The 'app' object will be discovered by Gunicorn
-app = create_app(os.getenv('FLASK_CONFIG') or 'app.config.Config')
-
-# Apply database migrations automatically
-with app.app_context():
-    try:
-        upgrade()
-        print("✅ Database upgraded successfully.")
-    except Exception as e:
-        print(f"❌ Failed to apply migrations: {e}")
+app = create_app(os.getenv('FLASK_CONFIG') or 'app.config.Config') 
